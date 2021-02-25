5aeaa053e9dfd89e08069434c527f516f07d40da
kotaro_uchida <kotaro1658@gmail.com>
2021-02-25 06:53:09 +0000

    static-pages html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,12 @@
 class StaticPagesController < ApplicationController
+  def first
+  end
+  def second
+  end
+  def contents
+  end
+  def entry
+  end
+  def form
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+ root 'static_pages/first'
+ get  'static_pages/first'
+ get  'static_pages/second'
+ get  'static_pages/contents'
+ get  'static_pages/entry'
+ get  'static_pages/form'
 end
